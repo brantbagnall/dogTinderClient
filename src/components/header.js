c2dc4152cc69b3117f68a7cba3508e789e1a0d9f
Brant Bagnall <brant.bagnall@gmail.com>
2017-10-06 10:00:24 -0600

    initial commit again

---
+++ src/components/header.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+    constructor(){
+        super()
+
+        this.state({
+
+        })
+    }
+
+    render(){
+      return(
+          <div>
+              test
+          </div>
+      )  
+    }
+}
+
+export default Header;
